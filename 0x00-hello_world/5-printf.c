c5fc813f1b95437564317027acbd2124b930d8a9
papaperidamax <papaperidamax@gmail.com>
2023-02-17 09:44:32 -0800

    return value

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description:'prinf function'
+ *
+ *return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
