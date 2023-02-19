1a92032fc799f926dff84e51fbcc2164f018a2af
papaperidamax <papaperidamax@gmail.com>
2023-02-19 05:36:55 -0800

    combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - combination of single digits numbers
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	putchar(n);
+
+	if (n != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
