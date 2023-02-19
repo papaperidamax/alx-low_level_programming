bd2828069ae7ffde5a0bfecea2f2fdb3f181996f
papaperidamax <papaperidamax@gmail.com>
2023-02-19 03:44:27 -0800

    decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - printing of decimal numbers
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	for (num = 0; num < 10; num++)
+	printf(num);
+	print('\n');
+	return (0);
+}
